0df4c3be5f3b6673bdeea6754b2015f743f7434f
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-03-09 17:01:25 +0100

    Display available data

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import fs from 'fs'
 
 // Create a new express app
 const app = express()
+
+let gameData = fs.readFileSync('./public/api/game/943.json')
+let parsedData = JSON.parse(gameData)
+
+// console.log(parsedData)
 
 // Set the view engine of the app to ejs
 app.set('view engine', 'ejs')
@@ -9,9 +16,21 @@ app.set('view engine', 'ejs')
 app.set('views', './views')
 // Set the location of the public folder with static files
 app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', async (req, res) => {
-    res.render('index')
+    let data = parsedData
+    // console.log(data)
+
+    if (data.hasStatistics === true) {
+        let gameStats = fs.readFileSync(`./public/api/game/${data.gameId}/statistics.json`)
+        let parsedStats = JSON.parse(gameStats)
+
+        res.render('index', {data, parsedStats})
+    } else {
+        res.render('index', {data})
+    }    
 })
 
 app.set('port', process.env.PORT || 8000)
